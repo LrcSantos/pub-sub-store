6a6ba6ad162f7601b923bb64ef43115f01728489
Lucas Santos <rafaelcostalucas3@gmail.com>
2025-05-22 18:15:18 -0300

    Tarefa prática #2 - EXTRA

--- services/report/app.js
+++ services/report/app.js
@@ -4,6 +4,7 @@ const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
 var report = {}
+
 async function updateReport(products) {
     for(let product of products) {
         if(!product.name) {
@@ -14,17 +15,32 @@ async function updateReport(products) {
             report[product.name]++;
         }
     }
-
 }
 
 async function printReport() {
+    console.log("\n===== RELATÓRIO DE VENDAS =====")
     for (const [key, value] of Object.entries(report)) {
         console.log(`${key} = ${value} vendas`);
-      }
+    }
+    console.log("===============================\n")
+}
+
+async function processMessage(msg) {
+    const deliveryData = JSON.parse(msg.content)
+    try {
+        await updateReport(deliveryData.products)
+        await printReport()
+        console.log("✔ Relatório atualizado com sucesso!")
+    } catch (error) {
+        console.log(`X Erro ao processar relatório: ${error}`)
+    }
 }
 
 async function consume() {
-    //TODO: Constuir a comunicação com a fila 
+    console.log(`INSCRITO COM SUCESSO NA FILA: ${process.env.RABBITMQ_QUEUE_NAME}`)
+    await (await RabbitMQService.getInstance()).consume(process.env.RABBITMQ_QUEUE_NAME, (msg) => {
+        processMessage(msg)
+    })
 } 
 
 consume()
